bb10c4f625c6e3d034af43044120896a19aaccf1
Anila Hoxha <51361033+anila-a@users.noreply.github.com>
2021-04-22 00:04:24 +0200

    Update bashscript4.sh

--- Linux/bashscript4.sh
+++ Linux/bashscript4.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 RES1=`ls -1 *.c | tail -2`
+# -1 indicates working in separate lines, -2 for last two files
 echo "The last two C files are $RES1"
 RES2=`ls -1 *.txt | tail -1`
 echo "The last TXT file is $RES2"
